74ace28f6a12e0592ba83d2c3396988cb671911b
Peter Tatkowski <tatp22@gmail.com>
2021-10-18 17:13:40 +0200

    first version of the GAT

---
+++ fast-gat/GAT.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.checkpoint import checkpoint
+
+class GraphAttentionHead(nn.Module):
+    """
+    A single head for the graph attention network.
+    Currently, this implementation is slow because of the
+    for loop.
+    """
+    def __init__(self, input_dim, output_dim):
+        self.w = nn.Linear(input_dim, output_dim)
+        self.a = nn.Linear(2*output_dim, output_dim)
+
+    def forward(self, nodes, edges):
+        nodes = self.w(nodes)
+        for curr_node, neighbors in edges.items():
+            pass
+        return None # TODO!
+
+class GraphAttentionLayer(nn.Module):
+    """
+    A layer of the Graph Attention Network.
+    NOTE! This is a different implementation than the original
+    paper. Instead of averaging, this applies a learnable layer of
+    size KFxF, more closely resembling the original attention paper.
+    """
+    def __init__(self, heads, input_dim, intermediate_dim, output_dim):
+        self.num_heads = heads
+        self.heads = nn.ModuleList()
+        self.final_layer = nn.Linear(heads*intermediate_dim, output_dim)
+        for head_no in heads:
+            self.heads.extend([GraphAttentionHead(input_dim, intermediate_dim)])
+
+    def forward(self, nodes, edges):
+        head_outputs = []
+        for head in self.heads:
+            head_outputs.append(head(nodes, edges))
+        out = torch.cat(head_outputs, dim=-1)
+        out = self.final_layer(out).softmax(dim=-1)
+        return out
+
+class GraphAttentionNetwork(nn.Module):
+    """
+    The entrance for the Graph attention Network.
+    This assumes that the inputs are:
+    nodes: An NxF array, with N nodes and F dimensions for the features
+    edges: A dictonary that represents directed edges. In particular, it has the form
+    {x: [y,z]}
+    which means that x has an edge to y and z. The edges do not repeat.
+    Also, the edges are expected to be 0 indexed and in integer form.
+
+    This function just returns the nodes, as the edges are unchanged.
+    """
+    def __init__(self, depth, heads, input_dim, inner_dim):
+        self.layers = nn.ModuleList()
+        if depth == 1:
+            inner_dim = input_dim
+
+        for layer_no in range(depth):
+            if layer_no == 0:
+                self.layers.extend([GraphAttentionLayer(heads, input_dim, inner_dim, inner_dim)])
+            elif layer_no == depth-1:
+                self.layers.extend([GraphAttentionLayer(heads, inner_dim, inner_dim, input_dim)])
+            else:
+                self.layers.extend([GraphAttentionLayer(heads, inner_dim, inner_dim, inner_dim)])
+
+
+    def forward(self, nodes, edges):
+        for layer in self.layers:
+            nodes = layer(nodes, edges)
+        return nodes
